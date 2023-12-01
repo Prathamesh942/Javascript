cf6333fe1611067c8677336e0d67ba9ab8d51a99
Prathamesh942 <87293621+Prathamesh942@users.noreply.github.com>
2023-12-01 13:42:17 +0000

    learned about history of javascript and variables

---
+++ Fundamentals/variables.js
@@ -0,0 +1,19 @@
+/*
+A variable is a “named storage” for data. 
+
+*/
+
+let name = 'prathamesh';
+let age;
+age=19;
+
+let user = 'john', married=true;
+
+/*
+Variable naming
+There are two limitations on variable names in JavaScript:
+The name must contain only letters, digits, or the symbols $ and _.
+The first character must not be a digit.
+*/
+
+const g = 9.8;
---
+++ Introduction/Into.js
@@ -0,0 +1,12 @@
+/*
+JavaScript was initially created to “make web pages alive”. 
+
+Today, JavaScript can execute not only in the browser, but also on the server, or actually on any device that has a special program called the JavaScript engine.
+
+The browser has an embedded engine sometimes called a “JavaScript virtual machine”.
+
+Modern JavaScript is a “safe” programming language. It does not provide low-level access to memory or the CPU, because it was initially created for browsers which do not require it.
+
+JavaScript on a webpage may not read/write arbitrary files on the hard disk, copy them or execute programs. It has no direct access to OS functions.
+
+*/

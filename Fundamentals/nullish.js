8d7ca7237af6b095bcaaeb6d3caeec14e2679f57
Prathamesh942 <87293621+Prathamesh942@users.noreply.github.com>
2023-12-11 08:33:18 +0000

    Learned nullish coalescing operator

---
+++ Fundamentals/nullish.js
@@ -0,0 +1,8 @@
+//Nullish coalescing operator
+//The nullish coalescing operator is written as two question marks ??
+
+// The result of a ?? b is:
+// if a is defined, then a, defined = not null or not undefined
+// if a isn’t defined, then b
+
+

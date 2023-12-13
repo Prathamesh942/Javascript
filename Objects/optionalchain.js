5b3cc098eed2ba1597e904c2ff47f0cdc42d9bab
Prathamesh942 <87293621+Prathamesh942@users.noreply.github.com>
2023-12-13 04:07:08 +0000

    optional chaining - to avoid accessing properties of objects that doesn't exist and avoiding error to get undefined

---
+++ Objects/optionalchain.js
@@ -0,0 +1,4 @@
+// The optional chaining ?. stops the evaluation if the value before ?. is undefined or null and returns undefined
+// user?.address?.street
+
+// The optional chaining ?. is not an operator, but a special syntax construct, that also works with functions and square brackets.

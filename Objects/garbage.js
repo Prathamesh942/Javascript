496f2851de341dc294c81f70ddf7242f1a9ebe4e
Prathamesh942 <87293621+Prathamesh942@users.noreply.github.com>
2023-12-13 03:28:28 +0000

    learned how garbage collection works in js

---
+++ Objects/garbage.js
@@ -0,0 +1,5 @@
+//The main concept of memory management in JavaScript is reachability.
+//There’s a background process in the JavaScript engine that is called garbage collector.It 
+//monitors all objects and removes those that have become unreachable.
+
+//The basic garbage collection algorithm is called “mark-and-sweep”.

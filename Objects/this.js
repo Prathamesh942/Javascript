6840a7edf1a88cbe8bdefb39100b3ccfb2ba0b33
Prathamesh942 <87293621+Prathamesh942@users.noreply.github.com>
2023-12-13 03:28:44 +0000

    learned use of 'this' keyword

---
+++ Objects/this.js
@@ -0,0 +1,12 @@
+//A function that is a property of an object is called its method.
+//user = {
+//  sayHi() { // same as "sayHi: function(){...}"
+//    alert("Hello");
+//  }
+//};
+//shorthand
+
+//To access the object, a method can use the 'this' keyword.
+//this do not look at object definition. Only the moment of call matters.
+
+//to chain methods return 'this' from methods of object
